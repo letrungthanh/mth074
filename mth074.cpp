0608dfa045a7b6b29261d99924d5df542d377838
TaiL-Bmt <trongtaile1990@gmail.com>
2022-04-02 21:08:55 +0700

    Tai: Add two test cases

--- mth074.cpp
+++ mth074.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, char *argv[])
 {
+    {
+        mth074::testOverflow();
+        mth074::testNumberCannotBePresentedByDouble();
+    }
     { // basic arithmetic
         std::cout << -1_mpz << std::endl;
         std::cout << +1_mpz << std::endl;
@@ -16,21 +20,21 @@ int main(int argc, char *argv[])
 
     { // calculate very big fibonacci number
         std::cout << mth074::fibonacci::calc_digits(100'000) << std::endl;
-        std::cout << mth074::fibonacci::calc(100'000) << std::endl;
+        //std::cout << mth074::fibonacci::calc(100'000) << std::endl;
     }
 
     { // co-prime
-        std::cout << mth074::coprime(5210644015679228794060694325390955853335898483908056458352183851018372555735221_mpz,
-                                     531137992816767098689588206552468627329593117727031923199444138200403559860852242739162502265229285668889329486246501015346579337652707239409519978766587351943831270835393219031728127_mpz)
-                  << std::endl;
+//        std::cout << mth074::coprime(5210644015679228794060694325390955853335898483908056458352183851018372555735221_mpz,
+//                                     531137992816767098689588206552468627329593117727031923199444138200403559860852242739162502265229285668889329486246501015346579337652707239409519978766587351943831270835393219031728127_mpz)
+//                  << std::endl;
     }
 
     { // quick power
-        std::cout << mth074::power(mth074::fibonacci::calc(100'000), 10) << std::endl;
+        //std::cout << mth074::power(mth074::fibonacci::calc(100'000), 10) << std::endl;
     }
 
     { // calculate greatest common divisor between very big fibonacci numbers
-        std::cout << mth074::gcd(mth074::fibonacci::calc(100'000), mth074::fibonacci::calc(200'0000)) << std::endl;
+        //std::cout << mth074::gcd(mth074::fibonacci::calc(100'000), mth074::fibonacci::calc(200'0000)) << std::endl;
     }
 
     return 0;
--- mth074.h
+++ mth074.h
@@ -96,6 +96,83 @@ namespace mth074
         return p;
     }
 
+    // Tai
+    void testOverflow()
+    {
+        std::cout << "\ntestOverflow\n";
+        std::cout << "expected is 2^64\n";
+        uint64_t num1 {1u << 64}; // Overflow: result is 0
+        std::cout << "uint64_t: " << num1 << std::endl;
+        bigint_t num2 {(bigint_t)1 << 64};
+        std::cout << "bigint lib: " << num2 << std::endl;
+    }
+
+    void testNumberCannotBePresentedByDouble()
+    {
+        std::cout << "\ntestNumberCannotBePresentedByDouble\n";
+        std::cout << "Number: 2^53 cannot displayed by double\n";
+        std::cout << "2^53 + 1 = 9007199254740992 + 1\n";
+        double num {pow(2, 53)};
+        std::cout << "uint64_t: " << static_cast<uint64_t>(num)  + 1 << std::endl;
+        std::cout << "double: " << static_cast<uint64_t>(num + 1) << std::endl;
+        std::cout << "bigint: " << bigint_t(num) + 1 << std::endl; 
+    }
+
+    void testTimeExecution()
+    {
+//        std::cout << "=======================================\n";
+//        std::cout << "testTimeExecution\n";
+//
+//        // Generate Input
+//        constexpr uint64_t numberOfSamples = 1e6;
+//        std::vector<bigint> leftValues;
+//        std::vector<bigint> rightValues;
+//        srand(time(0));
+//        for(int i = 0; i < numberOfSamples; i++)
+//        {
+//            leftValues.emplace_back(rand());
+//            rightValues.emplace_back(rand());
+//        }
+//
+//        std::vector<bigint> result(leftValues.size(), 0);
+//
+//        // Addition operator
+//        auto start = std::chrono::high_resolution_clock::now();
+//        std::ios_base::sync_with_stdio(false);
+//        for (int i = 0; i < leftValues.size(); i++)
+//        {
+//            result[i] = leftValues[i] + rightValues[i];
+//        }
+//        auto end = std::chrono::high_resolution_clock::now();
+//        double timeTaken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+//        timeTaken /= 1000000;
+//        std::cout << "Addition: " << timeTaken << std::endl;
+//
+//        // Substraction oprator
+//        start = std::chrono::high_resolution_clock::now();
+//        std::ios_base::sync_with_stdio(false);
+//        for (int i = 0; i < leftValues.size(); i++)
+//        {
+//            result[i] = leftValues[i] - rightValues[i];
+//        }
+//        end = std::chrono::high_resolution_clock::now();
+//        timeTaken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+//        timeTaken /= 1000000;
+//        std::cout << "Substraction: " << timeTaken << std::endl;
+//
+//        // Multiplication opeartor
+//        start = std::chrono::high_resolution_clock::now();
+//        std::ios_base::sync_with_stdio(false);
+//        for (int i = 0; i < leftValues.size(); i++)
+//        {
+//            result[i] = leftValues[i] * rightValues[i];
+//        }
+//        end = std::chrono::high_resolution_clock::now();
+//        timeTaken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+//        timeTaken /= 1000000;
+//        std::cout << "Multiplication: " << timeTaken << std::endl;
+    }
+
 } // namespace mth074
 
 #endif
